aa0e7ca81763482b7b0d690e3f08dbec19bf113f
Wagner Junior <101050460+owagnerjr@users.noreply.github.com>
2022-10-30 11:25:51 -0300

    Update Atividade 2 Exercicio 3.py

--- Atividade 2 Exercicio 3.py
+++ Atividade 2 Exercicio 3.py
@@ -1,5 +1,4 @@
 # -*-coding:latin1-*-
-# -*-coding:latin1-*-
 a = float(input('Digite o Primeiro Numero: '))
 b = float(input('Digite o Segundo  Numero: '))
 c = float(input('Digite o Terceiro Numero: '))
